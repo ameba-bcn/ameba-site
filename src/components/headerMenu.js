7f16dd4da4c5bac1f0f50d9ad7de00f4593f4d9b
Hector Pascual <hector.pascual.diaz@gmail.com>
2020-09-08 15:56:41 +0200

    a

--- src/components/headerMenu.js
+++ src/components/headerMenu.js
@@ -1,112 +1,106 @@
 import React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import LoginForm from './login';
-import { Link as LinkScroll } from "react-scroll";
-import MenuSpacer from './layout/menuTopSpacer';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import {NavLink, withRouter}  from 'react-router-dom'
 
+// const useStyles = makeStyles((theme) => ({
+//     // root: {
+//     //     flexGrow: 1,
+//     // },
+//     // menuButton: {
+//     //     marginRight: theme.spacing(2),
+//     // },
+//     title: {
+//         flexGrow: 1,
+//     },
+// }));
 
-function Menu() {
+export default function MenuAppBar() {
+    // const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const getNavLinkClass = (path) => { //creado para estilar la pestaña activa
+        // return this.props.location.pathname === path ? 'active' : '';
+    }
+
     return (
-        <div>
-            <Navbar bg="light" expand="lg" id="navbar">
-                <Navbar.Brand href="\ameba-site\">AMEBA</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-
-                        <NavItem>
-                            <LinkScroll activeClass="active"
-                                to="asso"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                ASSOCIACIÓ
-                        </LinkScroll>
-                        </NavItem>
-
-                        <NavItem>
-                            <LinkScroll activeClass=""
-                                to="activitats"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                ACTIVITATS
-                        </LinkScroll>
-                        </NavItem>
-                        
-                        <NavItem>
-                            <LinkScroll activeClass="active"
-                                to="shop"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                BOTIGA
-                        </LinkScroll>
-                        </NavItem>
-
-                        <NavItem>
-                            <LinkScroll activeClass=""
-                                to="locals"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                #SUPPORTYOURLOCALS
-                        </LinkScroll>
-                        </NavItem>
-
-                        <NavItem>
-                            <LinkScroll activeClass=""
-                                to="festi"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                PARKFEST
-                        </LinkScroll>
-                        </NavItem>
-                        
-                        <NavItem >
-                            <LinkScroll activeClass=""
-                                to="colabo"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                COLABORADORS
-                        </LinkScroll>
-                        </NavItem>
-
-                        <NavItem>
-                            <LinkScroll activeClass=""
-                                to="contacte"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}>
-                                CONTACTE
-                        </LinkScroll>
-                        </NavItem>
-           
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown> */}
-
-                    </Nav>
-                    <LoginForm />
-                </Navbar.Collapse>
-            </Navbar>
-            <MenuSpacer />
+        <div className="">
+            {/* <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup> */}
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className="" color="inherit" aria-label="menu">
+                        {/* <MenuIcon /> */}
+                        <NavLink to="/" >AMEBA</NavLink>
+          </IconButton>
+                    <Typography variant="h6" className="">
+                        <ul className="nav navbar-nav navbar-expand-lg">
+                            <li className={getNavLinkClass("/")}><NavLink to="/Activitats" >ACTIVITATS</NavLink></li>
+                            <li className={getNavLinkClass("/agregar")}><NavLink to="/Botiga">BOTIGA</NavLink></li>
+                            <li className={getNavLinkClass("/agregar")}><NavLink to="/Article">#SUPPORTYOURLOCALS</NavLink></li>
+                        </ul>
+                    </Typography>
+                    {auth && (
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
-
-
-export default Menu;
