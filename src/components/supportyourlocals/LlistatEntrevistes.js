701ad4ff3ae7ed23fbb68718c7b88a0fcd08c670
Hache <hector.pascual.diaz@gmail.com>
2020-12-10 16:24:51 +0100

    Parts implemented

--- src/components/supportyourlocals/LlistatEntrevistes.js
+++ src/components/supportyourlocals/LlistatEntrevistes.js
@@ -7,32 +7,34 @@ import './LlistatEntrevistes.css';
 
 export default function llistatEntrevistes() {
 
-    const cardClicked = (data) =>  {
-        return  <Redirect  to={`/Support/Entrevista?='${data.id}`} />
+    const cardClicked = (data) => {
+        return <Redirect to={`/Support/Entrevista?='${data.id}`} />
     }
 
     const cardGenerator = data.map((data) => {
         return (
-            <div className="fullcard" key={data.id} 
-            onClick={() => cardClicked(data)}>
-                <NavLink style={{ textDecoration: 'none' }} 
-                to={{pathname:'/support/'+data.id,
-                aboutProps: data}}>
-                <div className="card-support">
-                    <img className="card-support-img-top" src={data.img} alt={data.title} />
-                    <div className="card-support-body">
-                        <h5 className="card-support-title" >{data.title}</h5>
-                        <p className="card-support-text">{data.interview.intro}</p>
-                        <p className="card-support-text"><small className="text-muted">{data.date}</small></p>
+            <div className="fullcard" key={data.id}
+                onClick={() => cardClicked(data)}>
+                <NavLink style={{ textDecoration: 'none' }}
+                    to={{
+                        pathname: '/support/' + data.id,
+                        aboutProps: data
+                    }}>
+                    <div className="cardSupport">
+                        <img className="cardSupportImgTop" src={data.img} alt={data.title} />
+                        <div className="cardSupportTitle">
+                            {data.title}
+                        </div>
+                        <div className="cardSupportPlusBox">+</div>
+                        <div className="cardTagBox">DJ</div>
                     </div>
-                </div>
                 </NavLink>
             </div>
         )
     })
 
     return (
-        <div className="card-support-deck">
+        <div className="cardSupportDeck">
             {cardGenerator}
         </div>
     )
