fad2d24f84fe5f0cfc22a38668c9183d59c53e37
Hache <hector.pascual.diaz@gmail.com>
2021-02-03 20:19:49 +0100

    updated cart component

--- src/redux/services/cart.services.js
+++ src/redux/services/cart.services.js
@@ -4,21 +4,39 @@ import axios from "axios";
 const API_URL = "http://localhost/api/";
 
 const addInCart = (id) => {
+
+    if (JSON.parse(localStorage.getItem("user"))?.access !== undefined) {
+        console.log("Hay un user loggeado, nene")
+        return axios.post(API_URL + 'carts/',
+            {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))?.access}`
+                },
+                items: [id]
+            }).then((response) => {
+                console.log("cart response", response)
+                localStorage.setItem("cart", JSON.stringify(response.data.id));
+                return response.data;
+            })
+    } else {
+        console.log("No lo hay y hay que crear uuid")
+        return axios.put(API_URL + 'carts/current/', {
+            items: [id]
+        }).then((response) => {
+            console.log("cart response", response)
+            localStorage.setItem("cart", JSON.stringify(response.data.id));
+            return response.data;
+        })
+    }
     // Hay cart uuid en LS?
-    
+
     // No? -->     User Logged?
     //             No? --> axios.post(url, {items})
     //             Si? --> axios.???
 
     // Si? -->     axios.put(url/uuid, {items})
     // error --> ???
-    return axios.post(API_URL + 'carts/', {
-        items: [id]
-    }).then((response) => {
-        console.log("cart response", response)
-        localStorage.setItem("cart", JSON.stringify(response.data.id));
-        return response.data;
-    })
+
 };
 
 export default {
