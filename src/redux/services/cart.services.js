95b7f469cbf86469ec784dd83b72506f630143b8
Hache <hector.pascual.diaz@gmail.com>
2021-02-23 18:05:57 +0100

    Redux cart store implemented

--- src/redux/services/cart.services.js
+++ src/redux/services/cart.services.js
@@ -7,17 +7,27 @@ const addInCart = (id) => {
 
     if (JSON.parse(localStorage.getItem("user"))?.access !== undefined) {
         console.log("Hay un user loggeado, nene")
-        return axios.patch(API_URL + 'carts/current/',
-            {
-                headers: {
-                    Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))?.access}`
-                },
-                items: [id]
-            }).then((response) => {
-                console.log("cart response", response)
-                localStorage.setItem("cart_id", JSON.stringify(response.data.id));
-                return response.data;
-            })
+        return axios.get(API_URL + 'carts/current/', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))?.access}`
+            }
+        }).then((response) => {
+            console.log("cart response Important", response)
+            localStorage.setItem("cart_id", JSON.stringify(response.data.id));
+            // localStorage.setItem("cart_items", JSON.stringify(response.data.cart_items));
+            let cart_prev = response.data.cart_items;
+            cart_prev.push(id)
+            return axios.patch(`${API_URL}carts/${response.data.id}/`,
+                {
+                    headers: { Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))?.access}` },
+                    "items": cart_prev
+                }).then((response) => {
+                    console.log("cart response", response)
+                    localStorage.setItem("cart_id", JSON.stringify(response.data.id));
+                    localStorage.setItem("cart_items", JSON.stringify(cart_prev));
+                    return response.data;
+                })
+        })
     } else {
         // console.log("URGGG", localStorage.getItem("cart"))
         if (JSON.parse(localStorage.getItem("cart_id")) === null) {
@@ -36,7 +46,6 @@ const addInCart = (id) => {
                     return response;
                 })
             })
-            // falta patch para añadir al carro
         } else {
             let cart_uuid = JSON.parse(localStorage.getItem("cart_id"));
             let cart_prev = JSON.parse(localStorage.getItem("cart_items"));
@@ -54,7 +63,35 @@ const addInCart = (id) => {
     }
 };
 
+const removeItemCart = (id) => {
+    let cart_uuid = JSON.parse(localStorage.getItem("cart_id"));
+    let cart_prev = JSON.parse(localStorage.getItem("cart_items"));
+    const index = cart_prev.indexOf(id);
+    if (index > -1) {
+        cart_prev.splice(index, 1);
+    }
+    return axios.patch(`${API_URL}carts/${cart_uuid}/`,
+        { "items": cart_prev }
+    ).then((response) => {
+        console.log("cart response", response)
+        localStorage.setItem("cart_id", JSON.stringify(response.data.id));
+        localStorage.setItem("cart_items", JSON.stringify(cart_prev));
+        return response.data;
+    })
+}
+
+const deleteFullCart = () => {
+    let cart_uuid = JSON.parse(localStorage.getItem("cart_id"))
+    return axios.delete(`${API_URL}carts/${cart_uuid}`, {
+        headers: {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))?.access}`
+        }
+    })
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    addInCart
+    addInCart,
+    deleteFullCart,
+    removeItemCart
 };
