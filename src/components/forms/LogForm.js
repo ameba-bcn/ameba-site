f9406e576d7b34fbabfe50910b93c56e77e25961
Hache <hector.pascual.diaz@gmail.com>
2021-04-05 20:55:10 +0200

    AW-124 New LogForm created

---
+++ src/components/forms/LogForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LogForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/forms/MembershipForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MembershipForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
