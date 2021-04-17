7d465d2bb1d0d1253cb4079972c6f768406666af
Hache <hector.pascual.diaz@gmail.com>
2021-04-18 00:26:42 +0200

    LoginConfirmation logic implemented

--- src/pages/LogMailConfirmation.js
+++ src/pages/LogMailConfirmation.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from "react-redux";
 import { validateEmail } from './../redux/actions/auth';
-
 import { useHistory } from 'react-router-dom'
 
 export default function LogMailConfirmation(props) {
-    // http://localhost/activate/?token=22222
 
     const dispatch = useDispatch();
-
+    const [hasQueryParams, setHasQueryParams] = React.useState(false);
     const history = useHistory();
     const queryString = require('query-string');
     const parsed = queryString.parse(props.location.search);
     const strToken = parsed.token
 
-    const confirmed = (strToken) => {
-        dispatch(validateEmail(strToken))
-        history.push('/');
-    }
-    // new URLSearchParams(this.props.location.search).get("__firebase_request_key")
-    console.log("queryparamsss", parsed)
+    useEffect(() => {
+        if (strToken) {
+            dispatch(validateEmail(strToken))
+            setHasQueryParams(true)
+            setTimeout(() => {
+                history.push('/');
+            }, 3000);
+        }
+    }, [strToken]);
+
     return (
         <div className="loginWall">
-            <br />
-            <p>T'hem enviat un mail de confirmació, confirma l'enllaç</p>
-            <button className="print" onClick={() =>confirmed(strToken)}>Confirma mail</button>
+            {hasQueryParams ?
+                <p>Compte verificat: Benvingut!</p> :
+                <p>T'hem enviat un mail de confirmació, confirma l'enllaç</p>}
         </div>
     )
 }
--- src/pages/PasswordRecovery.js
+++ src/pages/PasswordRecovery.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { useDispatch } from "react-redux";
+import { passwordRecovery } from './../redux/actions/auth';
+import { useHistory } from 'react-router-dom'
 import PasswordRecoverForm from './../components/forms/PasswordRecoverForm'
 
-export default function PasswordRecovery() {
+export default function PasswordRecovery(props) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [hasQueryParams, setHasQueryParams] = React.useState(false);
+    const queryString = require('query-string');
+    const parsed = queryString.parse(props.location.search);
+    const strToken = parsed.token
+
+    useEffect(() => {
+        if (strToken) {
+            dispatch(passwordRecovery(strToken))
+            setHasQueryParams(true)
+            setTimeout(() => {
+                history.push('/');
+            }, 3000);
+        }
+    }, [strToken]);
 
     return (
         <div className="loginWall">
-            <PasswordRecoverForm />
+            {hasQueryParams ?
+                <p>Contrasenya recuperada</p> :
+                <PasswordRecoverForm />}
         </div>
     )
 }
