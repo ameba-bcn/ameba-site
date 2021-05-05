89b053fdeabd7dfd0308d4616a8ea42c678bcf09
Hache <hector.pascual.diaz@gmail.com>
2021-05-05 21:53:35 +0200

    Profile Dropdown created

---
+++ src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LettersMove from './../components/layout/LettersMove';
+
+export default function Profile() {
+    return (<>
+        <div>
+            Vista Perfil, cargar algunas tablejas, noseque
+        </div>
+            <LettersMove
+            className="lettersMoveDiv"
+            sentence="FES-TE SOCI/A "
+            color="#EB5E3E"
+          />
+        </>
+    )
+}
