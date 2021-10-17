12962a7580ada6c2cb2ead45ec027867bca3f422
Hache <hector.pascual.diaz@gmail.com>
2021-10-17 15:04:34 +0200

    [AW-292] - Profile view implemented

--- src/GlobalStyles.style.js
+++ src/GlobalStyles.style.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const Paragraph1 = styled.div`
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   color: #1d1d1b;
   font-size: 1.2rem;
-`
+`;
+
+export const RowSeparator = styled.div`
+  width: 85%;
+  height: 2px;
+  border-bottom: 2px solid black;
+  margin: 5px auto;
+`;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,20 +3,22 @@ import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LettersMove from "./../components/layout/LettersMove";
 import MemberProfile from "../components/profile/MemberProfile";
-import MembershipFormReadOnly from "../components/forms/MembershipForm/MembershipFormReadOnly";
+import { isEmptyObject } from "../utils/utils";
 
 export default function Profile() {
   const auth = useSelector((state) => state.auth);
-  const { user_member_data } = useSelector((state) => state.auth);
+  const { user_member_data, user_data } = useSelector((state) => state.auth);
   const { isLoggedIn = false } = auth;
-
+  const { username = "" } = user_data;
+  const isMember = !isEmptyObject(user_member_data);
   if (!isLoggedIn) {
     return <Redirect to="/" />;
   }
+  console.log("user_member_data", user_member_data);
   return (
     <div className="logViewYellow">
-      <div className="profileTitle">{!!user_member_data ?`Hola ${user_member_data?.first_name}!`:"Hola!"}</div>
-      {!!user_member_data ? <MemberProfile /> : <MembershipFormReadOnly />}
+      <div className="profileTitle">{`Hola ${username}!`}</div>
+      <MemberProfile isMember={isMember} />
       <LettersMove
         className="lettersMoveDiv"
         sentence="FES-TE SOCI/A "
