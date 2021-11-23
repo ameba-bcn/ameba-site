31eab24d167c1f3ca57cd1ab886e047a7554fdbd
Hache <hector.pascual.diaz@gmail.com>
2021-11-24 00:23:34 +0100

    [AW-314] - Load image while video is loading

--- src/components/main/Associacio.js
+++ src/components/main/Associacio.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import { useSelector } from "react-redux";
 import LettersMove from "./../layout/LettersMove";
 import "./Associacio.css";
-import {isCORSInactive} from "./../../utils/utils";
+import { isCORSInactive } from "./../../utils/utils";
 import { MOBILE_NORMAL } from "../../utils/constants";
 import { useMediaQuery } from "@material-ui/core";
 
 function Associacio() {
   const isMobile = useMediaQuery(MOBILE_NORMAL);
+  const [isReady, setIsready] = useState(false);
   const data = useSelector((state) => state.data);
   const { cover = [] } = data;
-  const mediaIndex = isMobile? 1 : 0;
+  const mediaIndex = isMobile ? 1 : 0;
   const { file = "" } = cover[mediaIndex] || {};
-  const urlMedia = isCORSInactive()+file;
+  const urlMedia = isCORSInactive() + file;
+  const staticImg = isMobile
+    ? "/AmebaPortadaMobile.jpg"
+    : "/AmebaPortadaDesktop.jpg";
 
   return (
     <div className="Bloque" id="associacio">
       <div className="cover-box">
+        {/* Loading a image meanwhile is loading video */}
+        {!isReady && (
+          <img
+            src={process.env.PUBLIC_URL + staticImg}
+            className="portadaFallback"
+            alt=""
+          />
+        )}
         <ReactPlayer
           url={urlMedia}
           playing={true}
@@ -26,6 +38,8 @@ function Associacio() {
           width="100%"
           height="auto"
           controls={false}
+          onReady={() => setIsready(true)}
+          onError={() => setIsready(false)}
         />
       </div>
       <LettersMove
