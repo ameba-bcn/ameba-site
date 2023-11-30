72c9dd8f04fca4708a5d553a1f3015dce3fbf46c
PascualHache <hector.pascual.diaz@gmail.com>
2023-11-30 16:20:44 +0100

    [fix/eslint]

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": 0,
+  },
+};
