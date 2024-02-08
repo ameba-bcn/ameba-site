030a3d3ba495143c035c618fe0690f0cd64d2cbb
PascualHache <hector.pascual.diaz@gmail.com>
2024-02-08 19:49:19 +0100

    [test/env-var-pt4]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,jsx,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
+  extends: ["plugin:flowtype/recommended", "airbnb", "prettier"],
+  plugins: ["flowtype", "prettier", "react-hooks"],
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2016,
     sourceType: "module",
     ecmaFeatures: {
-      modules: true,
       jsx: true,
+      experimentalObjectRestSpread: true,
     },
   },
-  plugins: ["react", "react-hooks"],
+  settings: {
+    "import/resolver": "webpack",
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  globals: {
+    cy: true,
+  },
   rules: {
-    "react/prop-types": 0,
+    // Configure rules here
+    quotes: [0, "single", { avoidEscape: true }],
+    "arrow-body-style": ["error", "as-needed"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off",
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    // Disable rules here
+    "jsx-a11y/click-events-have-key-events": "off",
+    "import/prefer-default-export": "off",
+    "import/no-named-as-default": "off",
+    "jsx-a11y/no-autofocus": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "react/jsx-boolean-value": "off",
+    "no-prototype-builtins": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/destructuring-assignment": "off",
+    "import/no-cycle": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-closing-tag-location": "off",
+    "no-param-reassign": 0,
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
   },
 };
