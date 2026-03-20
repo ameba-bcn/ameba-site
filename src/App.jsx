a2bd99d88814da1756d25dc8902a816a3eb107cc
PascualHache <hector.pascual.diaz@gmail.com>
2026-03-20 15:48:06 +0100

    removed loadables

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, createContext } from "react";
+import React, { Suspense, useState, useMemo, useEffect, createContext } from "react";
 import useUIStore from "./stores/useUIStore";
 import useProfileStore from "./stores/useProfileStore";
 import useAuthStore from "./stores/useAuthStore";
@@ -12,28 +12,31 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-image-gallery/styles/image-gallery.css";
 import FullscreenCheckout from "./fullscreenCheckout/FullscreenCheckout";
+import FullscreenSpinner from "./components/spinner/FullscreenSpinner";
+import lazyWithRetry from "./utils/lazyWithRetry";
 import "./App.css";
-import HomeLoadable from "./pages/home/Home.loadable";
-import AgendaLoadable from "./pages/agenda/Agenda.loadable";
-import BotigaLoadable from "./pages/Botiga.loadable";
-import SociosDetailedLoadable from "./pages/socios/components/SociosDetailed.loadable";
-import SociosLoadable from "./pages/socios/Socios.loadable";
-import GalleryLoadable from "./pages/gallery/Gallery.loadable";
-import LogSessionLoadable from "./pages/LogSession.loadable";
-import PasswordRecoveryLoadable from "./pages/PasswordRecovery.loadable";
-import CheckoutPageLoadable from "./pages/CheckoutPage.loadable";
-import MembershipsLoadable from "./pages/memberships/Memberships.loadable";
-import SendEmailPasswordRecoveryLoadable from "./pages/SendEmailPasswordRecovery.loadable";
-import QrClientLoadable from "./pages/QrClient.loadable";
-import ValidateEmailLoadable from "./pages/ValidateEmail.loadable";
-import LogMailConfirmationLoadable from "./pages/LogMailConfirmation.loadable";
-import ExternalEventsLoadable from "./pages/external-events/ExternalEvents.loadable";
-import ProfileLoadable from "./pages/profile/Profile.loadable";
-import CheckoutFinishedLoadable from "./pages/landing/CheckoutFinished.loadable";
-import SubscriptionFinishedLoadable from "./pages/landing/SubscriptionFinished.loadable";
-import LegalLoadable from "./pages/legal/Legal.loadable";
-import QrLandingLoadable from "./pages/qr-landing/QrLanding.loadable";
-import NotFoundLoadable from "./pages/NotFound.loadable";
+
+const Home = lazyWithRetry(() => import("./pages/home/Home"));
+const Agenda = lazyWithRetry(() => import("./pages/agenda/Agenda"));
+const Botiga = lazyWithRetry(() => import("./pages/Botiga"));
+const SociosDetailed = lazyWithRetry(() => import("./pages/socios/components/SociosDetailed"));
+const Socios = lazyWithRetry(() => import("./pages/socios/Socios"));
+const Gallery = lazyWithRetry(() => import("./pages/gallery/Gallery"));
+const LogSession = lazyWithRetry(() => import("./pages/LogSession"));
+const PasswordRecovery = lazyWithRetry(() => import("./pages/PasswordRecovery"));
+const CheckoutPage = lazyWithRetry(() => import("./pages/CheckoutPage"));
+const Memberships = lazyWithRetry(() => import("./pages/memberships/Memberships"));
+const SendEmailPasswordRecovery = lazyWithRetry(() => import("./pages/SendEmailPasswordRecovery"));
+const QrClient = lazyWithRetry(() => import("./pages/QrClient"));
+const ValidateEmail = lazyWithRetry(() => import("./pages/ValidateEmail"));
+const LogMailConfirmation = lazyWithRetry(() => import("./pages/LogMailConfirmation"));
+const ExternalEvents = lazyWithRetry(() => import("./pages/external-events/ExternalEvents"));
+const Profile = lazyWithRetry(() => import("./pages/profile/Profile"));
+const CheckoutFinished = lazyWithRetry(() => import("./pages/landing/CheckoutFinished"));
+const SubscriptionFinished = lazyWithRetry(() => import("./pages/landing/SubscriptionFinished"));
+const Legal = lazyWithRetry(() => import("./pages/legal/Legal"));
+const QrLanding = lazyWithRetry(() => import("./pages/qr-landing/QrLanding"));
+const NotFound = lazyWithRetry(() => import("./pages/NotFound"));
 
 const UserContext = createContext(null);
 
@@ -100,39 +103,41 @@ function App() {
       <UserContext.Provider value={value}>
         <div className="app-main-view">
           <ScrollTop showBelow={250} />
-          <Routes>
-            <Route path="/activitats" element={<AgendaLoadable />} />
-            <Route path="/botiga" element={<BotigaLoadable />} />
-            <Route path="/socis/:id" element={<SociosDetailedLoadable />} />
-            <Route path="/socis" element={<SociosLoadable />} />
-            <Route path="/gallery" element={<GalleryLoadable />} />
-            <Route path="/login" element={<LogSessionLoadable />} />
-            <Route path="/recovery" element={<PasswordRecoveryLoadable />} />
-            <Route path="/checkout" element={<CheckoutPageLoadable />} />
-            <Route path="/memberships" element={<MembershipsLoadable />} />
-            <Route
-              path="/send-recovery"
-              element={<SendEmailPasswordRecoveryLoadable />}
-            />
-            <Route path="/member-card" element={<QrClientLoadable />} />
-            <Route path="/validate-email" element={<ValidateEmailLoadable />} />
-            <Route path="/activate" element={<LogMailConfirmationLoadable />} />
-            <Route path="/product" element={<ExternalEventsLoadable />} />
-            <Route path="/profile/:id" element={<ProfileLoadable />} />
-            <Route path="/profile" element={<ProfileLoadable />} />
-            <Route
-              path="/summary-checkout"
-              element={<CheckoutFinishedLoadable />}
-            />
-            <Route
-              path="/subscribe"
-              element={<SubscriptionFinishedLoadable />}
-            />
-            <Route path="/legal" element={<LegalLoadable />} />
-            <Route path="/" element={<HomeLoadable />} />
-            <Route path="/qr-view" element={<QrLandingLoadable />} />
-            <Route path="*" element={<NotFoundLoadable />} />
-          </Routes>
+          <Suspense fallback={<FullscreenSpinner />}>
+            <Routes>
+              <Route path="/activitats" element={<Agenda />} />
+              <Route path="/botiga" element={<Botiga />} />
+              <Route path="/socis/:id" element={<SociosDetailed />} />
+              <Route path="/socis" element={<Socios />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/login" element={<LogSession />} />
+              <Route path="/recovery" element={<PasswordRecovery />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/memberships" element={<Memberships />} />
+              <Route
+                path="/send-recovery"
+                element={<SendEmailPasswordRecovery />}
+              />
+              <Route path="/member-card" element={<QrClient />} />
+              <Route path="/validate-email" element={<ValidateEmail />} />
+              <Route path="/activate" element={<LogMailConfirmation />} />
+              <Route path="/product" element={<ExternalEvents />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route
+                path="/summary-checkout"
+                element={<CheckoutFinished />}
+              />
+              <Route
+                path="/subscribe"
+                element={<SubscriptionFinished />}
+              />
+              <Route path="/legal" element={<Legal />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/qr-view" element={<QrLanding />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </div>
       </UserContext.Provider>
       <Contacte />
