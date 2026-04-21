23d6b45678f631396374e591334230789cb810b0
PascualHache <hector.pascual.diaz@gmail.com>
2026-04-21 21:00:48 +0200

    [feat/elimina-banner-fstvl26]

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Associacio from "./views/cover/Associacio";
 import Activitats from "./views/agenda/Activitats";
 import Manifesto from "./views/manifesto/Manifesto";
 import LettersMove from "../../components/layout/LettersMove";
-import Banner from "../../components/banner/Banner";
 import { useTranslation } from "react-i18next";
 
 export default function Home() {
@@ -11,12 +10,6 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="HomeContent">
-        <Banner
-          image="/AMEBAFSTVL26banner.png"
-          link="https://dice.fm/event/yoanwr-ameba-fstvl-2026-18th-apr-la-terrrazza-barcelona-poble-espanyol-barcelona-carpa-picnic-poble-espanyol-barcelona-tickets"
-          alt="AMEBA FSTVL 2026"
-          title="AMEBA FSTVL 2026"
-        />
         <Associacio />
         <Manifesto />
         <Activitats />
